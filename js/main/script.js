cae5489f26ff740ff62008b40cccb13477f52742
maxmotsyk <21mechanic21@gmail.com>
2021-12-03 22:12:51 +0300

    add two scroll

--- js/main/script.js
+++ js/main/script.js
@@ -1,6 +1,52 @@
 $(document).ready(function () {
-    $(".btn").click(function () {
-        $("#sosi").toggle("slow");
-    });
+
+   
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+
+      $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+      
+      });
+
+      $('.slider_oil').slick({
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: "<img src='../img/main/section/prevArrow.png'  class='prev' alt='1'>",
+        nextArrow: "<img src='../img/main/section/nextArrow.png'  class='next' alt='2'>",
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
 });
 
