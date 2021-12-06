48bd08adbb95fa34376a83b34ae76dbb4ced191e
maxmotsyk <21mechanic21@gmail.com>
2021-12-06 20:28:25 +0300

    commit

--- js/main/script.js
+++ js/main/script.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+  
+    $(".undermenu").click(function(){
+      $(this.childNodes[1].childNodes[1]).toggleClass('active');
+    
+      let this_element = this.childNodes[3];
+      let under_menus = $('.undermenu');
+
+      if($(this_element).hasClass('active')){
+        $(this_element).toggleClass('active').slideUp(300);
+      }
+      else{
+        $(this_element).toggleClass('active').slideDown(300);
+      }
+
+      console.log(under_menus.length);
+      for( let i = 0; i < under_menus.length; i++){
+
+        if(under_menus[i] !== this){
+          
+          if($(under_menus[i].childNodes[3]).hasClass('active')){
+
+            $(under_menus[i].childNodes[3]).toggleClass('active').slideUp(300);
+            $(under_menus[i].childNodes[1].childNodes[1]).toggleClass('active');
+          }
+
+        }
+
+      }
+
+    });
 
    
     $('.slider-for').slick({
@@ -16,6 +46,8 @@ $(document).ready(function () {
         dots: false,
         centerMode: true,
         focusOnSelect: true,
+        prevArrow: "<img src='../img/main/section/prevArrow.png'  class='prev' alt='1'>",
+        nextArrow: "<img src='../img/main/section/nextArrow.png'  class='next' alt='2'>",
       
       });
 
