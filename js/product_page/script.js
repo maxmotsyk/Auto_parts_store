43a83790a189593707f5dfea76c5389cd472e928
maxmotsyk <21mechanic21@gmail.com>
2022-01-19 23:37:43 +0300

    add filter body

--- js/product_page/script.js
+++ js/product_page/script.js
@@ -79,6 +79,57 @@ $(document).ready(function () {
 
     });
 
+    $('.button_for_ul_drop_list').click(function(){
+      let drop_menu_mass = $('.button_for_ul_drop_list');
+      
+      if($(this).children('img').hasClass('active')){
+        $(this).next().slideUp(300);
+        $(this).children('img').toggleClass('active');
+      }
+      else{
+        $(this).next().slideDown(300);
+        $(this).children('img').toggleClass('active');
+      }
+
+    });
+
+
+    $('.live_search').keyup(function(){
+
+      let live_word = $(this).val().trim().toLowerCase();
+      let mass_li_item = $(this).next().children('li');
+
+      if(live_word !== ''){
+        mass_li_item.each(function(){
+          if($(this).children('p').text().toLowerCase().search(live_word) == -1){
+            $(this).fadeOut(300);
+          }
+          else{
+            $(this).fadeIn(300);
+          }
+
+        })
+      }
+      else{
+        mass_li_item.each(function(){
+          $(this).fadeIn(300);
+        })
+      }
+
+    })
+
+    $('.option_ul_item').click(function(){
+
+      if($(this).children('input').is(':checked')){
+        $(this).children('input').prop('checked', false);
+      }
+      else{
+        $(this).children('input').prop('checked', true);
+      }
+
+
+    });
+
 
       $('.footer_menu_category').click(function(){
 
@@ -95,5 +146,42 @@ $(document).ready(function () {
           }
         }
       });
+
+      const ratings = $('.rating_star');
+
+      if(ratings.length > 0){
+        initRatings();
+      }
+
+      // фунция иницелизации рейтингов
+      function initRatings(){
+        let rating_active,rating_value;
+
+        // заполнения всех рейтингов
+        for(let i = 0; i < ratings.length; i++){
+          const rating = ratings[i];
+          initRating(rating);
+        }
+
+        // заполнения определеного рейтинга
+        function initRating(rating){
+          initRatingVars(rating);
+          setRatingActiveWidth();
+        }
+
+        function initRatingVars(rating){
+          rating_active = $(rating).find('.rating_active');
+          rating_value = $(rating).children('.rating_value');
+          console.log($(rating_active));
+
+        }
+
+        function setRatingActiveWidth(){
+          let index = $(rating_value).text().trim()
+          let rating_active_width = index / 0.05;
+          $(rating_active).css("width",`${rating_active_width}%`);
+        }
+
+      }
 
 });
